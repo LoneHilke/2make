e372deda72783a2eb2320272b3fcede796e95018
LMH <90649822+LoneHilke@users.noreply.github.com>
2024-08-08 21:27:06 +0200

    skal have tjek på </div>
    Har fået image til at virke
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- krea/make/views.py
+++ krea/make/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import View
 # Create your views here.
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'make/base.html')
